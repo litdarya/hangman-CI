dd293903ecb51320ebb67001a1d626c0ed1d0347
Darya Litvintseva <litv.daria@gmail.com>
2019-09-28 23:38:00 +0300

    add test with full game checking

--- hangman/__init__.py
+++ hangman/__init__.py
@@ -1,63 +1,89 @@
 import os
 import csv
 import random
-from sys import argv
+import sys
 import numpy as np
 
 
 class Game:
-    def __init__(self, words_path, tries=5):
-        assert os.path.exists(words_path)
+    """
+    Hangman game class
+    """
+    # pylint: disable=too-many-instance-attributes
+
+    def __init__(self, words_path=None, tries=5):
         self.words_path = words_path
         self.words = []
         self.play_word = None
+        self.play_word_lst = None
+        self.guess_word = None
         self.max_tries = tries
+        # these members are needed for mocking in test
+        self.input_stream = sys.stdin
+        self.output_stream = sys.stdout
 
         self._read_words()
 
     def _read_words(self):
+        if self.words_path is None:
+            return
+
+        assert os.path.exists(self.words_path)
+
         with open(self.words_path, 'r') as csv_file:
             reader = csv.reader(csv_file, delimiter='\n')
             for row in reader:
                 self.words.append(row[0].strip())
 
     def pick_random(self):
-        return random.choice(self.words)
+        self.play_word = random.choice(self.words)
+        self.play_word_lst = np.array(list(self.play_word.strip()))
+        self.guess_word = np.array(['*'] * len(self.play_word))
+
+    def check_guess(self, guess):
+        ans = True
+        if guess in self.play_word_lst:
+            idx = np.where(self.play_word_lst == guess)
+            self.guess_word[idx] = guess
+        else:
+            ans = False
+
+        tmp_word = ''.join(self.guess_word)
+        return ans, tmp_word
 
     def start_game(self):
-        self.play_word = self.pick_random()
-        word = np.array(list(self.play_word.strip()))
-        guess_word = np.array(['*']*len(self.play_word))
+        self.pick_random()
         tries = 0
-        tmp_word = ''
+        curr_word = ''
 
-        while tries < self.max_tries:
-            guess = input("Guess a letter:\n").strip()
+        print("Guess a letter:\n", file=self.output_stream)
+        for line in self.input_stream:
+            guess = line.strip()
+            correct, curr_word = self.check_guess(guess)
 
-            if guess in word:
-                idx = np.where(word == guess)
-                guess_word[idx] = guess
-                print("Hit!\n")
+            if correct:
+                print("Hit!\n", file=self.output_stream)
             else:
                 tries += 1
-                print(f"Missed, mistake {tries} out of {self.max_tries}.\n")
+                print(f"Missed, mistake {tries} out of {self.max_tries}.\n",
+                      file=self.output_stream)
 
-            tmp_word = ''.join(guess_word)
-            print(f"The word: {tmp_word}\n")
-            if tmp_word == self.play_word:
+            print(f"The word: {curr_word}\n", file=self.output_stream)
+            if curr_word == self.play_word or tries >= self.max_tries:
                 break
+            print("Guess a letter:\n", file=self.output_stream)
 
-        if tmp_word == self.play_word:
-            print("You won!")
+        if curr_word == self.play_word:
+            print("You won!", file=self.output_stream)
             return True
 
-        print("You lost!")
+        print("You lost!", file=self.output_stream)
         return False
 
 
 def main():
-    assert len(argv) == 2
-    game = Game(words_path=str(argv[1]))
+    assert len(sys.argv) == 2
+    game = Game(words_path=str(sys.argv[1]))
     game.start_game()
 
 
--- tests/test_hangman.py
+++ tests/test_hangman.py
@@ -1,6 +1,74 @@
+import io
 from hangman import Game
 
 
-def test_test():
-    game = Game(words_path='dict.txt')
-    assert game is not None
+def test_two_letters():
+    """
+    Checks if a guess is correct and there are more
+    than one guessed letter in a word, it changes all
+    """
+    game = Game()
+    game.words = ['kitty']
+    game.pick_random()
+    guess = 't'
+    _, word = game.check_guess(guess)
+    assert word == '**tt*'
+
+
+def test_bad_guess():
+    """
+    Checks if a guess is incorrect
+    """
+    game = Game()
+    game.words = ['game']
+    game.pick_random()
+    guess = 'q'
+    check, word = game.check_guess(guess)
+    assert not check
+    assert word == '****'
+
+
+def test_win():
+    """
+    Checks win scenario
+    """
+    game = Game()
+    game.words = ['game']
+    game.pick_random()
+    game.input_stream = [
+        'a',
+        'g',
+        'm',
+        'e',
+    ]
+    game.output_stream = io.StringIO()
+    check = game.start_game()
+    assert check
+    assert ''.join(game.guess_word) == 'game'
+    assert 'won' in game.output_stream.getvalue()
+    assert 'lost' not in game.output_stream.getvalue()
+
+
+def test_fail():
+    """
+    Checks lose scenario
+    """
+    game = Game()
+    game.words = ['game']
+    game.pick_random()
+    game.input_stream = [
+        'a',
+        'g',
+        'm',
+        'b',
+        'c',
+        'd',
+        'f',
+        'i',
+    ]
+    game.output_stream = io.StringIO()
+    check = game.start_game()
+    assert not check
+    assert ''.join(game.guess_word) == 'gam*'
+    assert 'lost' in game.output_stream.getvalue()
+    assert 'won' not in game.output_stream.getvalue()
